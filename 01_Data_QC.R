79f4e83e8127751dcfada644809ad8ddf2be8f63
ycl6 <ycl6.gel@gmail.com>
2020-09-02 17:31:26 +0100

    Add R package details

--- 01_Data_QC.R
+++ 01_Data_QC.R
@@ -24,7 +24,28 @@
 #' 
 #' -----
 #' 
+#' **scRNAseq:** [Bioconductor](https://bioconductor.org/packages/scRNAseq/)
+#' 
+#' **DropletUtils:** [Bioconductor](https://bioconductor.org/packages/DropletUtils/)
+#' 
+#' **scater:** [Bioconductor](https://bioconductor.org/packages/scater/), [GitHub](https://github.com/LTLA/scater), [Paper](https://doi.org/10.1093/bioinformatics/btw777)
+#' 
+#' **ggbeeswarm:** [CRAN](https://cran.r-project.org/package=ggbeeswarm), [GitHub](https://github.com/eclarke/ggbeeswarm)
+#' 
+#' **Demo Dataset:** `BachMammaryData` from *[Differentiation dynamics of mammary epithelial cells revealed by single-cell RNA sequencing](https://doi.org/10.1038/s41467-017-02001-5) Nat Commun. 2017;8(1):2128.*
+#' 
+#' **License:** GPL-3.0
+#' 
+#' 
 #' # Start R & prepare environment
+#' 
+#' ## Start `R`
+#' 
+#' ```
+#' cd /ngs/scRNA-seq-Analysis-Demo
+#' 
+#' R
+#' ```
 #' 
 #' ## Load R libraries
 #' 
